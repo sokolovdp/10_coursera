6ac359ca17a7145027309a7d8f0f86284c8475bc
Dmitrii Sokolov <sokolovdp@gmail.com>
2017-06-12 22:31:23 +0200

    corrected get_html_from_utl function None is used directly added CLI arguments corrected readme

--- README.md
+++ README.md
@@ -1,12 +1,18 @@
-# Coursera Dump
+# Purpose
 
-coursera.py loads information about courses from Coursera xml-feed 
-and saves short information into excel file "courses_info.xlsx"
+coursera.py loads courses description from Coursera xml-feed and saves short info to excel file 
 
-# usage sample
+# Usage sample
+```bazaar
+usage: coursera.py [--help] [--n N] [--file FILE]
 
-python coursera.py
+program saves info about N first courses from Coursera feed to excel FILE
 
+optional arguments:
+  --help       show this help message and exit
+  --n N        number of courses
+  --file FILE  file name
+```
 # Project Goals
 
 The code is written for educational purposes. Training course for web-developers - [DEVMAN.org](https://devman.org)
--- coursera.py
+++ coursera.py
@@ -1,3 +1,5 @@
+import argparse
+import sys
 from openpyxl import Workbook
 from openpyxl.utils.dataframe import dataframe_to_rows
 import urllib.request
@@ -5,48 +7,60 @@ from bs4 import BeautifulSoup
 import pandas as pd
 
 
-def get_url_soup(url_full):
+def get_html_from_url(url_full):
     try:
         response = urllib.request.urlopen(url_full)
+        return response.read()
     except urllib.error.URLError as e:
         print("open page error:", e.reason)
-    else:
-        html = response.read()
-        return BeautifulSoup(html, "lxml")
 
 
 def get_courses_list():
-    soup = get_url_soup("https://www.coursera.org/sitemap~www~courses.xml")
-    if soup:
+    html = get_html_from_url("https://www.coursera.org/sitemap~www~courses.xml")
+    if html is not None:
+        soup = BeautifulSoup(html, "lxml")
         return [url_full.text for url_full in soup.find_all('loc')]
 
 
 def get_course_info(course_url):
-    soup = get_url_soup(course_url)
-    if soup:
+    html = get_html_from_url(course_url)
+    if html is not None:
+        soup = BeautifulSoup(html, "lxml")
         title = soup.find('meta', attrs={'property': 'og:title'}).attrs['content'].replace(" | Coursera", "")
         start_date = soup.find('div', attrs={'class': 'startdate rc-StartDateString caption-text'}).text
         language = soup.find('div', attrs={'class': 'rc-Language'}).text
         duration = len(soup.find_all('div', attrs={'class': 'week-heading body-2-text'}))
         rating = soup.find_all('div', attrs={'class': 'ratings-text headline-2-text'})
-        if rating:
+        if rating is not None:
             rating = rating[0].contents[0].text
         else:
             rating = "No rating yet"
         return {'1_title': title, '2_date': start_date, '3_language': language, '4_weeks': duration, "5_rating": rating}
 
 
-def output_courses_info_to_xlsx(courses_data):
-    excel_filename = 'courses_info.xlsx'
+def output_courses_info_to_xlsx(courses_info, filename):
     book = Workbook()
     sheet = book.active
     sheet.title = "Coursera"
-    courses_data_frame = pd.DataFrame(courses_data)
-    for row in dataframe_to_rows(courses_data_frame, index=True, header=True):
+    courses_dataframe = pd.DataFrame(courses_info)
+    for row in dataframe_to_rows(courses_dataframe, index=True, header=True):
         sheet.append(row)
-    book.save(filename=excel_filename)
+    book.save(filename=filename)
+
+
+def main(filename, number_courses):
+    courses_urls = get_courses_list()
+    print("loaded info about {} courses".format(len(courses_urls)))
+    courses_info = [get_course_info(url) for url in courses_urls[:number_courses]]
+    output_courses_info_to_xlsx(courses_info, filename)
+    print("info {} courses saved in {}".format(number_courses, filename))
+
 
 if __name__ == '__main__':
-    courses_urls = get_courses_list()
-    courses_info = [get_course_info(url) for url in courses_urls[:3]]  # save info for first 20 courses
-    output_courses_info_to_xlsx(courses_info)
+    ap = argparse.ArgumentParser(
+        description='program saves info about N first courses from Coursera feed to excel FILE')
+    ap.add_argument("--n", dest="n", action="store", type=int, default=20, help="  number of courses")
+    ap.add_argument("--file", dest="file", action="store", default='courses_info.xlsx', help="  file name")
+    args = ap.parse_args(sys.argv[1:])
+
+    main(args.file, args.n)
